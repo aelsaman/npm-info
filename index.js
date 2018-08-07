01e4e4d597582e92541b4dd0b1a06762793ae0de
Alaa ElSaman <alaa.elsaman@gmail.com>
2018-08-07 11:09:38 +0400

    add last time modified to response

--- index.js
+++ index.js
@@ -1,62 +1,63 @@
-'use strict';
-var got = require('got');
-var registryUrl = require('registry-url');
-var Promise = require('pinkie-promise');
+"use strict";
+var got = require("got");
+var registryUrl = require("registry-url");
+var Promise = require("pinkie-promise");
 
-module.exports = function (name) {
-	if (typeof name !== 'string') {
-		return Promise.reject(new Error('package name required'));
+module.exports = function(name) {
+	if (typeof name !== "string") {
+		return Promise.reject(new Error("package name required"));
 	}
 
 	return got(registryUrl + name.toLowerCase())
-		.then(function (data) {
-			var name 		= '';
-			var version 	= '';
-			var description = '';
-			var license 	= '';
-			var homepage 	= '';
-			var authorName = '';
+		.then(function(data) {
+			var name = "";
+			var version = "";
+			var description = "";
+			var lastModified = "";
+			var license = "";
+			var homepage = "";
+			var authorName = "";
 
 			var dataParsed = JSON.parse(data.body);
 
-			name 		= dataParsed.name;
-			version 	= dataParsed[ 'dist-tags' ].latest;
+			name = dataParsed.name;
+			version = dataParsed["dist-tags"].latest;
 			description = dataParsed.description;
-			license 	= dataParsed.license;
+			lastModified = dataParsed.time.modified;
+			license = dataParsed.license;
 
-			if(dataParsed.homepage !== undefined){
-				homepage 	= dataParsed.homepage;
+			if (dataParsed.homepage !== undefined) {
+				homepage = dataParsed.homepage;
 			}
 
-			if(dataParsed.author !== undefined){
+			if (dataParsed.author !== undefined) {
 				authorName = dataParsed.author.name;
-			}
-			else{
-				if(dataParsed.maintainers !== undefined){
+			} else {
+				if (dataParsed.maintainers !== undefined) {
 					for (var i in dataParsed.maintainers) {
 						var maintainer = dataParsed.maintainers[i];
-						if(authorName === ''){
+						if (authorName === "") {
 							authorName = maintainer.name;
-						}
-						else{
-							authorName = authorName + ', ' + maintainer.name;
+						} else {
+							authorName = authorName + ", " + maintainer.name;
 						}
 					}
 				}
 			}
 
 			return {
-				name          	: name,
-				version			: version,
-				description		: description,
-				license			: license,
-				homepage		: homepage,
-				author			: authorName
+				name: name,
+				version: version,
+				description: description,
+				lastModified: lastModified,
+				license: license,
+				homepage: homepage,
+				author: authorName
 			};
 		})
-		.catch(function (err) {
+		.catch(function(err) {
 			if (err.statusCode === 404) {
-				err.message = 'Package doesn\'t exist';
+				err.message = "Package doesn't exist";
 			}
 
 			throw err;
